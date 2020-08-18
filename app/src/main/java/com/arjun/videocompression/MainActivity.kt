d8a6b0b2bccde05c7d60477d54881b3929c04814
iamarjun <arjunmanoj1995@gmail.com>
2020-08-18 22:18:04 +0530

    turn to alpha version of the apis for new onActivityResult handing

--- app/src/main/java/com/arjun/videocompression/MainActivity.kt
+++ app/src/main/java/com/arjun/videocompression/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.arjun.videocompression
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.arjun.videocompression.databinding.ActivityMainBinding
+import com.arjun.videocompression.util.viewBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the returned Uri
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.pickVideo.setOnClickListener {
+            getContent.launch("video/*")
+        }
+
     }
 }
