0a8b338821a0a48726cea4a37537e80fcac45134
iamarjun <arjunmanoj1995@gmail.com>
2020-08-18 22:50:04 +0530

    add exo player dependencies, pick video from local storage and play the same on the activity

--- app/src/main/java/com/arjun/videocompression/MainActivity.kt
+++ app/src/main/java/com/arjun/videocompression/MainActivity.kt
@@ -2,17 +2,32 @@ package com.arjun.videocompression
 
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.arjun.videocompression.databinding.ActivityMainBinding
 import com.arjun.videocompression.util.viewBinding
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
-    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-        // Handle the returned Uri
+    private lateinit var player: SimpleExoPlayer
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        Timber.d(it.toString())
+        val mediaSource = buildMediaSourceNew(it)
+        player.prepare(mediaSource, true, false)
     }
 
 
@@ -20,9 +35,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        player = ExoPlayerFactory.newSimpleInstance(
+                this,
+                DefaultRenderersFactory(this),
+                DefaultTrackSelector(),
+                DefaultLoadControl())
+
+
         binding.pickVideo.setOnClickListener {
             getContent.launch("video/*")
         }
 
+        binding.playerView.player = player
+
+    }
+
+    private fun buildMediaSourceNew(uri: Uri): MediaSource? {
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(this, Util.getUserAgent(this, getString(R.string.app_name)))
+        return ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
     }
 }
